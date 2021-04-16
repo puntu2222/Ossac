bd77c18ab78d2aaf78a8f96f0bb404af2b2b08c9
lenovo <lenovo@DESKTOP-5D2Q8GC>
2021-04-17 00:06:54 +0530

    commit all files

---
+++ src/main/java/com/ossac/in/OssacApplication.java
@@ -0,0 +1,13 @@
+package com.ossac.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OssacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ossac/in/OssacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ossac.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
