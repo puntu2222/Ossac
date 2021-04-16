4417066d8ea36e8003f726bb013d4cbadacfc50a
lenovo <lenovo@DESKTOP-5D2Q8GC>
2021-04-17 00:26:28 +0530

    commit costumer controller And costumer Entity class

---
+++ src/main/java/com/ossac/in/controller/CostumerContoller.java
@@ -0,0 +1,12 @@
+package com.ossac.in.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CostumerContoller {
+	
+	public void add() {
+		
+	}
+
+}
---
+++ src/main/java/com/ossac/in/entity/costumer/CostumerAddress.java
@@ -0,0 +1,8 @@
+package com.ossac.in.entity.costumer;
+
+import javax.persistence.Entity;
+
+@Entity
+public class CostumerAddress {
+
+}
